c01e9021318668e23e9e73af355d3a2b86352d5b
MontassarZammely <133285872+MontassarZammely@users.noreply.github.com>
2024-04-05 05:48:01 +0100

    Update UpdateState.js

--- routes/UpdateState.js
+++ routes/UpdateState.js
@@ -2,9 +2,11 @@
 const express = require('express');
 const router = express.Router();
 const updatestate = require('../controllers/updateState');
+const verifyRoles = require('../middleware/VerifyRoles');
 
-router.put('/demands/:id/accept', updatestate.updateDemandStateToAccepted);
-router.put('/demands/:id/decline', updatestate.updateDemandStateToDecline);
+
+router.put('/demands/:id/accept',verifyRoles, updatestate.updateDemandStateToAccepted);
+router.put('/demands/:id/decline',verifyRoles, updatestate.updateDemandStateToDecline);
 
 
 
