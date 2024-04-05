32bb6c0f976c69b8e0cd4551e864f7b6c21411e3
MontassarZammely <133285872+MontassarZammely@users.noreply.github.com>
2024-04-05 05:50:33 +0100

    UpdateState ADMINS barka

--- routes/UpdateState.js
+++ routes/UpdateState.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const updatestate = require('../controllers/updateState');
 const verifyRoles = require('../middleware/VerifyRoles');
 
-
+// ken les ADMINS ynajmou ybaadlou
 router.put('/demands/:id/accept',verifyRoles, updatestate.updateDemandStateToAccepted);
 router.put('/demands/:id/decline',verifyRoles, updatestate.updateDemandStateToDecline);
 
